94e96b169a16b045ce281f33a22813bfe860f05f
skywang2 <727854256@qq.com>
2022-02-10 01:03:26 +0800

    [chg][normal]1.add texture fragment shader

--- Assignment3/Code/main.cpp
+++ Assignment3/Code/main.cpp
@@ -112,7 +112,7 @@ Eigen::Vector3f texture_fragment_shader(const fragment_shader_payload& payload)
     if (payload.texture)
     {
         // TODO: Get the texture value at the texture coordinates of the current fragment
-
+        return_color = payload.texture->getColor(payload.tex_coords.x(), payload.tex_coords.y());
     }
     Eigen::Vector3f texture_color;
     texture_color << return_color.x(), return_color.y(), return_color.z();
@@ -141,6 +141,18 @@ Eigen::Vector3f texture_fragment_shader(const fragment_shader_payload& payload)
         // TODO: For each light source in the code, calculate what the *ambient*, *diffuse*, and *specular* 
         // components are. Then, accumulate that result on the *result_color* object.
 
+        Eigen::Vector3f v3f_light = light.position - point;
+        Eigen::Vector3f v3f_eye = eye_pos - point;
+        float r2 = light.position.squaredNorm();
+        //diffuse
+        float receivedRate = normal.normalized().dot(light.position.normalized());
+        result_color += kd.cwiseProduct((light.intensity/r2) * std::max(0.0f, receivedRate));
+        //specular
+        Eigen::Vector3f h = (v3f_light + v3f_eye).normalized();//bisector
+        float eyeReceivedRate = normal.normalized().dot(h);        
+        result_color += ks.cwiseProduct((light.intensity/r2) * std::pow(std::max(0.0f, eyeReceivedRate), p));
+        //ambient
+        result_color += ka.cwiseProduct(amb_light_intensity);
     }
 
     return result_color * 255.f;
